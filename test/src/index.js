f95b929edaa5c90d952e4f835bfa75964c596ddf
Boumarouane <i.boumarouane@gmail>
2020-04-11 23:01:40 +0200

    fin du projet

--- test/src/index.js
+++ test/src/index.js
@@ -1,94 +1,9 @@
 
-import React, { Component } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
-import * as THREE from "three";
+import Cube from './Cube'
 
-const style = {
-  height: 250 // we can control scene size by setting container dimensions
-};
-
-class App extends Component {
-
-  componentDidMount() {
-      this.sceneSetup()
-      this.addCustomSceneObject()
-      this.startAnimationLoop()
-      window.addEventListener('resize', this.handleWindowResize);
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('resize', this.handleWindowResize);
-    window.cancelAnimationFrame(this.requestID);
-    // this.controls.dispose();
-}
-
-  sceneSetup = () => {
-      const width = this.el.clientWidth;
-      const height = this.el.clientHeight;
-
-      this.scene = new THREE.Scene()
-      this.camera = new THREE.PerspectiveCamera(75, width/height, 0.1, 1000);
-
-      this.camera.position.z = 5;
-
-      this.renderer = new THREE.WebGLRenderer();
-      this.renderer.setSize(width, height);
-      this.el.appendChild(this.renderer.domElement);
-  }
-
-
-  addCustomSceneObject = () => {
-    const geometry = new THREE.BoxGeometry(2, 2, 2);
-    const material = new THREE.MeshPhongMaterial({
-      color: 0x156289,
-      emissive: 0x072534,
-      side: THREE.DoubleSide,
-      flatShading: true
-    });
-    this.cube = new THREE.Mesh(geometry, material);
-    this.scene.add(this.cube);
-
-    const lights = [];
-    lights[0] = new THREE.PointLight(0xffffff, 1, 0);
-    lights[1] = new THREE.PointLight(0xffffff, 1, 0);
-    lights[2] = new THREE.PointLight(0xffffff, 1, 0);
-
-    lights[0].position.set(0, 200, 0);
-    lights[1].position.set(100, 200, 100);
-    lights[2].position.set(-100, -200, -100);
-
-    this.scene.add(lights[0]);
-    this.scene.add(lights[1]);
-    this.scene.add(lights[2]);
-  };
-
-  
-  startAnimationLoop = () => {
-    this.cube.rotation.x += 0.01;
-    this.cube.rotation.y += 0.01;
-
-    this.renderer.render(this.scene, this.camera);
-
-    this.requestID = window.requestAnimationFrame(this.startAnimationLoop);
-  }
-
-  handleWindowResize = () => {
-    const width = this.el.clientWidth;
-    const height = this.el.clientHeight;
-
-    this.renderer.setSize(width, height);
-    this.camera.aspect = width / height;
-
-    this.camera.updateProjectionMatrix();
-  };
-
-  render() {
-    return (
-      <div style={style} ref={ref => (this.el = ref)} /> 
-      )
-  }
-}
-
+// Composant principal contenant la possibilité de monter et démonter notre projet three.js.
 class Container extends React.Component {
   state = { isMounted: true };
 
@@ -101,10 +16,9 @@ class Container extends React.Component {
             this.setState(state => ({ isMounted: !state.isMounted }))
           }
         >
-          {isMounted ? "Unmount" : "Mount"}
+          {isMounted ? "Démonter" : "Monter"}
         </button>
-        {isMounted && <App />}
-        {isMounted && <div>Scroll to zoom, drag to rotate</div>}
+        {isMounted && <Cube />}
       </>
     );
   }
